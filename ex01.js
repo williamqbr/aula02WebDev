3f0042f9925b548b021d99c19e6ac2df45877eb0
williamqbr <williamq2006@gmail.com>
2025-03-24 10:03:31 -0300

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,18 @@
+var nome = 'William ';
+var sobrenome = 'Queiroz';
+var anoAtual = 2025;
+var idade = 19;
+var ns = nome + sobrenome;
+var nascimento = anoAtual - idade;
+console.log(ns);
+console.log(nascimento);
+
+//inicio aula 03
+
+nomeInserido = prompt('Insira seu nome e sobrenome: ');
+console.log('O usuário ' + nomeInserido + ' logou no site');
+alert('Olá, ' + nomeInserido + '! Bem-vindo!');
+var idadeInserida = prompt('Insira sua idade');
+var idadeCorreta = parseint(idadeInserida);
+alert('Sua idade é' + idadeCorreta);
+console.log('O usuario tem ' + idadeCorreta + ' anos');
---
+++ main.js
@@ -0,0 +1,30 @@
+var cliente = 'Maria';
+var quantidade = 30;
+var preco = 50.5;
+var emEstoque = true;
+
+var numeroa = 1;
+var numerob = 2;
+var numeroc = 3;
+
+var soma = numeroa + numerob;
+var sub = numerob - numeroa;
+var multiplicao = numerob * numeroc
+var divisao = numeroc/numerob
+
+console.log(soma);
+console.log(sub);
+console.log(multiplicao);
+console.log(divisao);
+
+var textoa = 'WEB';
+var textob = 'DEV';
+var branco = ' ';
+
+var resultadoA = textoa + textob;
+var resultadoB = textob + 1;
+var resultadoC = textoa + branco + textob
+
+console.log(resultadoA);
+console.log(resultadoB);
+console.log(resultadoC);
